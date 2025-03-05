ac8b5acb372f2acd2aaaab92ed1e2a8140887142
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2025-03-05 16:51:31 -0300

    urls.py

--- Python com Django 5 - catalogofilmes/filmes/urls.py
+++ Python com Django 5 - catalogofilmes/filmes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.listar_filmes, name='listar_filmes'),
     path('<int:filme_id>/', views.detalhes_filme, name='detalhes_filme'),
+    path('filme/adicionar/', views.adicionar_filme, name='adicionar_filme'),
     path('<int:filme_id>/editar/', views.editar_filme, name='editar_filme'),
     path('<int:filme_id>/excluir/', views.excluir_filme, name='excluir_filme'),
 ]
